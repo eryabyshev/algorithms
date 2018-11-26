c2b972de5955af981c98dc8cdedd884adab457a6
Evgeny Ryabyshew <ru.evgeny.dev@gmail.com>
2018-11-26 23:45:18 +0300

    merge rep with acmp rep

---
+++ hackerrank/src/MagicSquare.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MagicSquare {
+
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int[] square = new int[9];
+        int sum = 0;
+
+        for(int i = 0; i < square.length; i++)
+            square[i] = in.nextInt();
+        Arrays.sort(square);
+
+        for(int i = 0; i < square.length;i++)
+            sum += Math.max(i + 1, square[i]) - Math.min(i + 1, square[i]);
+
+        System.out.println(sum);
+    }
+
+}
