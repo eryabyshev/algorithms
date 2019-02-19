744f994191eb8a8b6e85a836b7b607b0276a4a4f
Evgeny Ryabyshew <34579787+evgenyrybishew@users.noreply.github.com>
2019-02-19 17:34:52 +0300

    complete number of inversions

--- stepic/src/number/inversions/NumberOfInversions.py
+++ stepic/src/number/inversions/NumberOfInversions.py
@@ -1,56 +1,47 @@
+
+
 def read_input_date():
     input()
     array = input().split(" ")
     return array
 
-
-
 def main():
-    array = read_input_date()
-    num_of_inv = merge_sort(array)
-    print(num_of_inv)
+    merger = Merge()
+    print(merger.get_number_of_inversions(read_input_date()))
 
 
-def merge_sort(array, inv=0):
-    if len(array) == 1:
-        return 0
-    if(len(array) > 1):
-        mid = len(array) // 2
-        left_part = array[:mid]
-        right_part = array[mid:]
-        left = merge_sort(left_part, inv)
-        right = merge_sort(right_part, inv)
+class Merge:
+    def __int__(self):
+        self.__number_of_inversion = 0
 
-        i = 0
-        j = 0
-        k = 0
-        track = 0
-        while i < len(left_part) and j < len(right_part):
-            if left_part[i] <= right_part[j]:
-                array[k] = left_part[i]
-                track += 1
-                i += 1
-                k += 1
-
+    def __merge(self, left, right):
+        result = []
+        left_counter = 0
+        right_counter = 0
+        for i in range(len(left) + len(right)):
+            if right_counter == len(right) or (left_counter < len(left) and int(left[left_counter]) <= int(right[right_counter])):
+                result.append(left[left_counter])
+                left_counter += 1
             else:
-                array[k] = right_part[j]
-                inv = inv + (len(left_part) - track)
-                j += 1
-                k += 1
+                self.__number_of_inversion += len(left) - left_counter
+                result.append(right[right_counter])
+                right_counter += 1
+        return result
 
-        while i < len(left_part):
-            array[k] = left_part[i]
-            i += 1
-            k += 1
+    def sort(self, array):
+        if len(array) == 1:
+            return array
+        mid = len(array) // 2
+        left = array[:mid]
+        right = array[mid:]
+        left = self.sort(left)
+        right = self.sort(right)
+        return self.__merge(left, right)
 
-        while j < len(right_part):
-            array[k] = right_part[j]
-            j += 1
-            k += 1
-        return inv + left + right
+    def get_number_of_inversions(self, array):
+        self.__number_of_inversion = 0
+        self.sort(array)
+        return self.__number_of_inversion
+
 
 main()
-
-# 5
-#
-# 1 2 3 5 4
